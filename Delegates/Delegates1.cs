0e7a4d2d1ae6780ebb5db1eb996dc353d6597f97
jvnarayana <venkat.jadda1991@gmail.com>
2018-03-10 12:18:20 -0600

    Added Final Delegates POC solution

---
+++ Delegates/Delegates1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+
+    public enum WorkType
+    {
+        Reports,
+        Meetings
+    }
+    //Step1: Creating a Delegate
+    public delegate void WorkFromDelegate(int hours, WorkType worktype);
+
+    class Delegate1
+    {
+        static void Main(string[] args)
+        {
+            WorkFromDelegate del1 = new WorkFromDelegate(WorkFromHandlerMethod1);
+            WorkFromDelegate del2 = new WorkFromDelegate(WorkFromHandlerMethod2);
+            del1.Invoke(1, WorkType.Meetings);
+            del2.Invoke(12, WorkType.Reports);
+            Console.Read();
+        }
+
+        // Step 2: Creating a Handler method
+        static void WorkFromHandlerMethod1(int NumOfHours, WorkType workerType)
+        {
+
+            Console.WriteLine("Handler method1 :" + NumOfHours.ToString());
+        }
+        static void WorkFromHandlerMethod2(int hours, WorkType worktype)
+        {
+
+            Console.WriteLine("Handler method2 :" + hours.ToString());
+        }
+
+     
+    }
+
+}
---
+++ Delegates1/CustomDelegate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates1
+{
+
+    public enum WorkType
+    {
+        Reports,
+        Meetings
+    }
+    //Step1: Creating a Delegate
+    public delegate void WorkFromDelegate(int hours, WorkType worktype);
+
+    class CustomDelegate
+    {
+        static void Main(string[] args)
+        {
+            WorkFromDelegate del1 = new WorkFromDelegate(WorkFromHandlerMethod1);
+            WorkFromDelegate del2 = new WorkFromDelegate(WorkFromHandlerMethod2);
+            Work(del1);
+            Work(del2);
+            Console.Read();
+
+        }
+
+        // Step 2: Creating a Handler method
+static void WorkFromHandlerMethod1(int NumOfHours,WorkType workerType)
+        {
+
+            Console.WriteLine("Handler method1 :"+NumOfHours.ToString());
+        }
+        static void WorkFromHandlerMethod2(int hours, WorkType worktype)
+        {
+
+            Console.WriteLine("Handler method2 :" + hours.ToString());
+        }
+
+        //Step 3: Creating a common method 
+        static void Work(WorkFromDelegate del)
+        {
+            del(5,WorkType.Reports);
+        }
+
+    }
+
+
+
+}
